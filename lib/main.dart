367043938cb3bb25b1fe553865cc699428fbbc82
sohel7 <sohelocp@gmail.com>
2023-09-17 15:19:59 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,30 +7,77 @@ main(){
 
 class MyApp extends StatelessWidget{
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-   return  MaterialApp(
-     theme: ThemeData(primarySwatch: Colors.green),
-     darkTheme: ThemeData(primarySwatch: Colors.amber ),
-     color: Colors.green,
-     debugShowCheckedModeBanner: false,
-     home: HomeActivity(),
-   );
+    return  MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+      darkTheme: ThemeData(primarySwatch: Colors.blue ),
+      color: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: HomeActivity(),
+    );
   }
 }
+
 
 class HomeActivity extends StatelessWidget{
   const HomeActivity({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          ElevatedButton(onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity1()));
+           }, child: Text('Go Activity 1')),
 
+          ElevatedButton(onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity2()));
+           }, child: Text('Go Activity 2'))
+        ],
+      )
+    );
+  }
+}
+
+
+class Activity1 extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
-       appBar: AppBar(title: Text("Home"),),
-       body: Text("Hello WOrld"),
-     );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Activity1")
+      ),
+        body: Center(
+          child: ElevatedButton(onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=> Activity2()));
+          }, child: Text('Go Activity 2')),
+        )
+    );
   }
+}
 
+
+class Activity2 extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Activity2")
+      ),
+        body: Center(
+          child: ElevatedButton(onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity1()));
+          }, child: Text('Activity 1')),
+        )
+    );
+  }
 }
