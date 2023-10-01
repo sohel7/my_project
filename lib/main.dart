bb650c65b96baa7b8e4e2e3c0600d1be9de8acff
sohel7 <sohelocp@gmail.com>
2023-10-02 00:37:59 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -49,3 +49,86 @@
             ),
           ),
         ),
+
+//==================================== TEXT BUTTON===============================
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {
+                  // Add your button click logic here
+                },
+                child: Text(
+                  'Click Me',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  padding: EdgeInsets.all(16),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+// ======================================== OutlinedButton =============================================
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              OutlinedButton(
+                onPressed: () {
+                  // Add your button press logic here
+                },
+                child: Text('OutlinedButton'),
+                style: OutlinedButton.styleFrom(
+                  primary: Colors.blue, // Text color
+                  side: BorderSide(color: Colors.blue), // Border color
+                  backgroundColor: Colors.white, // Background color
+                  padding: EdgeInsets.all(16.0), // Padding
+                  textStyle: TextStyle(
+                    fontSize: 18.0, // Text size
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+//================================== Floating Action Button====================================
+
+  home: Scaffold(
+        appBar: AppBar(
+          title: Text('Floating Action Button Example'),
+        ),
+        body: Center(
+          child: Text(
+            'Press the button below!',
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Add your action here
+            print('Button pressed');
+          },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.blue, // Background color of the button
+          foregroundColor: Colors.white, // Icon color
+          tooltip: 'Add', // Tooltip text
+          elevation: 6, // Shadow depth
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(100), // Button shape
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat, // Button position
+      ),
+    );
+
