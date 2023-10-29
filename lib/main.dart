9185a4c856454cc86505b637dfa4e028b1452eee
sohel7 <sohelocp@gmail.com>
2023-10-29 10:56:47 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,63 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
-main(){
-  runApp( const MyApp());
-}
 
-class MyApp extends StatelessWidget{
-  const MyApp({super.key});
+onTap: (){
+              showDialog(context: context, builder: (context){
+               return AlertDialog(
+                 title: Text('Select Action'),
+                 content: Column(
+                   children: [
+                     ListTile(
+                       title: Text('Edit'),
+                     ),
+                     ListTile(
+                       title: Text('Delete'),
+                     )
+                   ],
+                 ),
+               );
+              });
+            },
+
+--================================== Full Code============================
+
+  import 'package:flutter/material.dart';
+
+class ProductListScreen extends StatelessWidget {
+  const ProductListScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.blue),
-      darkTheme: ThemeData(primarySwatch: Colors.blue ),
-      color: Colors.blue,
-      debugShowCheckedModeBanner: false,
-      home: HomeActivity(),
-    );
-  }
-}
-
-// Home Activite
-//====================================
-class HomeActivity extends StatelessWidget{
-   const HomeActivity({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // SCAFFOLD
-    //===================
-      return Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
+        title: Text('CRUD API'),
       ),
-      body: Center(
-        // Progress Linier
-        //==================
-      child: LinearProgressIndicator(
-          minHeight: 50,
-        color: Colors.orange,
-        backgroundColor: Colors.blue,
+      body: Column(
+        children: [
+          ListTile(
+            onTap: (){
+              showDialog(context: context, builder: (context){
+               return AlertDialog(
+                 title: Text('Select Action'),
+                 content: Column(
+                   children: [
+                     ListTile(
+                       title: Text('Edit'),
+                     ),
+                     ListTile(
+                       title: Text('Delete'),
+                     )
+                   ],
+                 ),
+               );
+              });
+            },
+            leading:Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0LgIPwB4gjYlOy5_YtiC7GSU5VJQVBgwG2w&usqp=CAU'),
+            title: Text('Product Name'),
+            subtitle: Text('Product Description'),
+            trailing: Text('\$120'),
+          )
+        ],
       ),
-      )
-
     );
-
   }
 }
-
